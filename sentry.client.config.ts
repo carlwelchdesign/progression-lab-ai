4f15494a9aecb84caa7e98b1767d65418314ec03
Carl Welch <carlwelchdesign@gmail.com>
2026-03-25 11:43:59 -0700

    added sentry

---
+++ sentry.client.config.ts
@@ -0,0 +1,17 @@
+import * as Sentry from '@sentry/nextjs';
+import { Replay } from '@sentry/replay';
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
+  debug: process.env.NODE_ENV !== 'production',
+  replaysSessionSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    new Replay({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+});
